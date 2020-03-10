1c01a49a43a1c35432b8bd5236aaf716768f09f9
Abhinav Gupta <guptabhi123@gmail.com>
2020-03-10 14:36:36 -0400

    first commit

---
+++ src/Assignment4/TrainHmm.java
@@ -0,0 +1,150 @@
+package Assignment4;
+
+import java.io.*;
+import java.util.*;
+
+public class TrainHmm {
+    public static String START = "Start";
+    public static String END = "End";
+    
+    public static void main(String[] args) throws Exception{
+        Map<String,Integer> stateIndexMap = new LinkedHashMap<>();
+        Map<String,Integer> wordIndexMap = new LinkedHashMap<>();
+        stateIndexMap.put(START, 1);
+        stateIndexMap.put(END, 2);
+        File file = new File("D:\\NYU_assignment\\Spring_2020\\NLP\\NLP\\src\\Assignment4\\WSJ_02-21.pos");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        String[] temp;
+        int stateIndex = 3;
+        int wordIndex = 0;
+        while ((line = br.readLine()) != null) {
+            if (!line.trim().equals("")) {
+                temp = line.split("\t");
+                String word = temp[0];
+                String tag = temp[1];
+                if(!wordIndexMap.containsKey(word)){
+                    wordIndexMap.put(word, wordIndex++);
+                }
+                if(!stateIndexMap.containsKey(tag)){
+                    stateIndexMap.put(tag, stateIndex++);
+                }
+            }
+        }
+
+        double[][] wordState = new double[wordIndexMap.size()][stateIndexMap.size()+1];
+        double[][] stateState = new double[stateIndexMap.size()+1][stateIndexMap.size()+1];
+        double[] stateMax = new double[stateIndexMap.size()+1];
+
+        br = new BufferedReader(new FileReader(file));
+        int stateIndexFrom = 0;
+        int stateIndexTo = 0;
+        wordIndex = 0;
+        String prevState = START;
+        while ((line = br.readLine()) != null) {
+            if (!line.trim().equals("")) {
+                temp = line.split("\t");
+                String word = temp[0];
+                String tag = temp[1];
+                if(wordIndexMap.containsKey(word)){
+                    wordIndex = wordIndexMap.get(word);
+                }
+                if(stateIndexMap.containsKey(tag)) {
+                    stateIndexFrom = stateIndexMap.get(prevState);
+                    stateIndexTo = stateIndexMap.get(tag);
+                    prevState = tag;
+                }
+                stateState[stateIndexFrom][0]++;
+                wordState[wordIndex][stateIndexTo]++;
+                stateState[stateIndexFrom][stateIndexTo]++;
+            } else if(prevState!=START){
+                stateIndexFrom = stateIndexMap.get(prevState);
+                stateIndexTo = stateIndexMap.get(END);
+                prevState = START;
+                stateState[stateIndexFrom][0]++;
+                stateState[stateIndexFrom][stateIndexTo]++;
+            }
+        }
+
+//        System.out.println("wordState");
+//        print2DArray(wordState, wordIndexMap, stateIndexMap);
+//        System.out.println("stateState");
+//        print2DArray(stateState, stateIndexMap, stateIndexMap);
+//        System.out.println(wordIndexMap);
+//        System.out.println(stateIndexMap);
+
+        calculateProbabilityState(stateState);
+        calculateProbabilityWord(stateState, wordState, stateIndexMap, stateMax);
+//
+//        System.out.println(stateState[stateIndexMap.get(".")][2]);
+//        System.out.println("wordState");
+//        print2DArray(wordState, wordIndexMap, stateIndexMap);
+//        System.out.println("stateState");
+//        print2DArray(stateState, stateIndexMap, stateIndexMap);
+        File fileTest = new File("D:\\NYU_assignment\\Spring_2020\\NLP\\NLP\\src\\Assignment4\\WSJ_24.pos");
+        File filePos = new File("D:\\NYU_assignment\\Spring_2020\\NLP\\NLP\\src\\Assignment4\\test_generate.pos");
+        processFile(stateState, wordState, stateIndexMap, wordIndexMap, fileTest, filePos);
+    }
+
+    public static void processFile(double[][] stateState, double[][] wordState, Map<String,Integer> stateIndexMap,
+                                   Map<String,Integer> wordIndexMap, File file, File filePos) throws Exception{
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        BufferedWriter brw = new BufferedWriter(new FileWriter(filePos));
+        String line;
+        List<String> obs = new LinkedList<>();
+        while ((line = br.readLine()) != null) {
+            if (!line.trim().equals("")) {
+                obs.add(line.split("\t")[0]);
+            } else {
+                String[] obsArr = obs.stream().toArray(String[] ::new);
+                obs = new LinkedList<>();
+                String[] tags = Veterbi.viterbi(stateState, wordState, stateIndexMap, wordIndexMap, obsArr);
+                for(int i = 0; i < tags.length; i++){
+                    brw.write(obsArr[i] + "\t" + tags[i] + "\n");
+                }
+                brw.write("\n");
+            }
+        }
+        brw.close();
+//        .split("\t")[0]
+    }
+
+    public static void calculateProbabilityState(double[][] stateState){
+        for(int i = 1; i < stateState.length; i++){
+            for(int j = 1; j < stateState[0].length; j++){
+                stateState[i][j] = stateState[i][j]/stateState[i][0];
+            }
+        }
+    }
+
+    public static void calculateProbabilityWord(double[][] stateState, double[][] wordState, Map<String,Integer> stateIndexMap, double[] stateMax){
+        for(Map.Entry<String, Integer> entry: stateIndexMap.entrySet()){
+            int stateIndex = entry.getValue();
+            for(int i = 0; i < wordState.length; i++){
+                wordState[i][stateIndex] = wordState[i][stateIndex]/stateState[stateIndex][0];
+                if(stateMax[stateIndex] < wordState[i][stateIndex]){
+                    stateMax[stateIndex] = wordState[i][stateIndex];
+                }
+            }
+        }
+    }
+
+    public static void print2DArray(double[][] arr, Map<String,Integer> map, Map<String,Integer> state){
+        System.out.println(state);
+        for(int i = 0; i< arr.length; i++){
+            System.out.println(getKeyByValue(map, i) + " " + Arrays.toString(arr[i]));
+        }
+        System.out.println();
+    }
+
+    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+        for (Map.Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+}
+
---
+++ src/Assignment4/Veterbi.java
@@ -0,0 +1,124 @@
+package Assignment4;
+
+import java.io.File;
+import java.util.*;
+
+import static Assignment4.TrainHmm.START;
+import static Assignment4.TrainHmm.END;
+
+class Pair{
+    double value;
+    Integer prev;
+    Pair(double value, Integer prev){
+        this.value = value;
+        this.prev = prev;
+    }
+    @Override
+    public String toString() {
+        return this.value + " " + this.prev;
+    }
+}
+
+public class Veterbi {
+    public static void main(String[] args) throws Exception{
+        Map<String, Integer> stateIndexMap = new LinkedHashMap<>();
+        stateIndexMap.put(START, 1);
+        stateIndexMap.put(END, 2);
+        stateIndexMap.put("noun", 3);
+        stateIndexMap.put("verb", 4);
+        double[][] stateState = new double[][]{
+                {0.0,0.0,0.0,0.0,0.0},
+                {0.0,0.0,0.0,0.8,0.2},
+                {0.0,0.0,0.0,0.0,0.0},
+                {0.0,0.0,0.1,0.1,0.8},
+                {0.0,0.0,0.7,0.2,0.1}};
+        Map<String, Integer> wordIndexMap = new LinkedHashMap<>();
+        wordIndexMap.put("fish", 0);
+        wordIndexMap.put("sleep", 1);
+        double[][] wordState = new double[][]{
+                {0.0,0.0,0.0,0.8,0.5},
+                {0.0,0.0,0.0,0.2,0.5}};
+
+        File fileTest = new File("D:\\NYU_assignment\\Spring_2020\\NLP\\NLP\\src\\Assignment4\\test.words");
+        File filePos = new File("D:\\NYU_assignment\\Spring_2020\\NLP\\NLP\\src\\Assignment4\\test_generate.pos");
+        TrainHmm.processFile(stateState, wordState, stateIndexMap, wordIndexMap, fileTest, filePos);
+    }
+
+    public static String[] viterbi(double[][] stateState, double[][] wordState, Map<String,Integer> stateIndexMap, Map<String,Integer> wordIndexMap, String[] obs){
+        Pair[][] v = new Pair[stateState.length][obs.length+2];
+        for(int i = 0; i<v.length; i++){
+            for(int j = 0; j < v[0].length;j++){
+                v[i][j] = new Pair(0,null);
+            }
+        }
+
+        v[1][0].value = 1;
+        for(int wordIndex = 1; wordIndex <= obs.length; wordIndex++){
+            String word = obs[wordIndex-1];
+            for(int stateIndex = 1; stateIndex < stateState[0].length; stateIndex++){
+                double maxProb = -1;
+                Integer maxStateIndex = -1;
+                for(int prevStateIndex = 1; prevStateIndex < stateState[0].length; prevStateIndex++){
+                    double wordProb;
+                    if(!wordIndexMap.containsKey(word)){
+                        wordProb = 0.5; //wordNotFound(wordState, stateIndex);
+                    } else {
+                        wordProb = wordState[wordIndexMap.get(word)][stateIndex];
+                    }
+                    double prob = stateState[prevStateIndex][stateIndex] * wordProb
+                            * v[prevStateIndex][wordIndex-1].value;
+                    if(prob>maxProb){
+                        maxProb = prob;
+                        maxStateIndex = prevStateIndex;
+                    }
+                }
+                v[stateIndex][wordIndex].value = maxProb;
+                v[stateIndex][wordIndex].prev = maxStateIndex;
+            }
+        }
+
+        int endStateIndex = stateIndexMap.get("End");
+        double maxProb = -1;
+        Integer maxStateIndex = -1;
+        for (int prevStateIndex = 1; prevStateIndex < stateState[0].length; prevStateIndex++) {
+            double prob = stateState[prevStateIndex][endStateIndex] * v[prevStateIndex][obs.length].value;
+            if (prob > maxProb) {
+                maxProb = prob;
+                maxStateIndex = prevStateIndex;
+            }
+        }
+        v[endStateIndex][obs.length+1].value = maxProb;
+        v[endStateIndex][obs.length+1].prev = maxStateIndex;
+
+//        for(int i = 0; i < v.length;i++){
+//            System.out.println(Arrays.toString(v[i]));
+//        }
+//        System.out.println();
+        String[] tags = new String[obs.length];
+        int val = obs.length-1;
+        Pair current = v[stateIndexMap.get("End")][obs.length+1];
+        int n = obs.length+1;
+        while(true){
+            if(val>=0)
+                tags[val] = TrainHmm.getKeyByValue(stateIndexMap, current.prev);
+            val--;
+            n--;
+            if(current.prev==null){
+                break;
+            }
+            current = v[current.prev][n];
+        }
+//        System.out.println(Arrays.toString(tags));
+        return tags;
+    }
+
+    public static double wordNotFound(double[][] wordState, int stateIndex){
+        double maxProb = -1.0;
+        for(int wordIndex = 0; wordIndex<wordState.length; wordIndex++){
+            if(wordState[wordIndex][stateIndex] > maxProb){
+                maxProb = wordState[wordIndex][stateIndex];
+            }
+        }
+        return maxProb;
+    }
+}
